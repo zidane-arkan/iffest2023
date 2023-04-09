6cdb8f194c35444b7ecc37c656fe39f286ee75da
ZildjianVito <zildjianvito07@gmail.com>
2023-04-09 14:18:44 +0700

    update index.html and css

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,58 @@
-
 // DUMMY DATA
 const classes = [
-    {
-        subTitle: 'Web Development',
-        title: 'Front-End Developer',
-        content: 'Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.'
-    },
-    {
-        subTitle: 'Web Development',
-        title: 'Back-End Developer',
-        content: 'Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.'
-    },
-    {
-        subTitle: 'Mobile Development',
-        title: 'Flutter Developer',
-        content: 'Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.'
-    },
-    {
-        subTitle: 'Android Development',
-        title: 'Kotlin Developer',
-        content: 'Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.'
-    }
+  {
+    subTitle: "Web Development",
+    title: "Front-End Developer",
+    content:
+      "Shape the digital world with your creativity and code. Join our team of frontend developers and bring stunning designs to life.",
+  },
+  {
+    subTitle: "Web Development",
+    title: "Back-End Developer",
+    content:
+      "Behind every great website, there's a backend developer powering its functionality. Lets make a difference by building scalable systems.",
+  },
+  {
+    subTitle: "Mobile Development",
+    title: "Flutter Developer",
+    content:
+      "Create stunning mobile apps with ease. Join our team of Flutter developers and bring your ideas to life!",
+  },
+  {
+    subTitle: "Android Development",
+    title: "Kotlin Developer",
+    content:
+      "Build powerful and efficient Android apps. Join our team of Kotlin developers and make an impact on the world of mobile technology.",
+  },
 ];
-const classParent = document.getElementById('classesParent');
+const classParent = document.getElementById("classesParent");
 
 classes.forEach((item) => {
-    const classImage = document.createElement('img');
-    classImage.src = "https://dummyimage.com/720x400";
-    classImage.className = "object-cover object-center w-full h-40 mb-6 rounded";
-    classImage.alt = "Classes";
-    const divParent = document.createElement('div');
-    divParent.className = "p-4 xl:w-1/4 md:w-1/2";
-    const divContent = document.createElement('div');
-    divContent.className = "p-6 bg-gray-800 rounded-lg bg-opacity-40";
-    const headingTitle = document.createElement('h2');
-    headingTitle.className = "mb-4 text-lg font-medium text-white title-font";
-    const headingSub = document.createElement('h3');
-    headingSub.className = "text-xs font-medium tracking-widest text-yellow-400 title-font";
-    const content = document.createElement('p');
-    content.className = "text-base leading-relaxed";
-    
-    headingTitle.textContent = item.title;
-    headingSub.textContent = item.subTitle;
-    content.textContent = item.content;
+  const classImage = document.createElement("img");
+  classImage.src = "https://dummyimage.com/720x400";
+  classImage.className = "object-cover object-center w-full h-40 mb-6 rounded";
+  classImage.alt = "Classes";
+  const divParent = document.createElement("div");
+  divParent.className = "p-4 xl:w-1/4 md:w-1/2";
+  const divContent = document.createElement("div");
+  divContent.className = "p-6 bg-gray-800 rounded-lg bg-opacity-40";
+  const headingTitle = document.createElement("h2");
+  headingTitle.className = "mb-4 text-lg font-medium text-white title-font";
+  const headingSub = document.createElement("h3");
+  headingSub.className =
+    "text-xs font-medium tracking-widest text-yellow-400 title-font";
+  const content = document.createElement("p");
+  content.className = "text-base leading-relaxed";
 
-    divContent.appendChild(headingSub);
-    divContent.appendChild(headingTitle);
-    divContent.appendChild(classImage);
-    divContent.appendChild(content);
+  headingTitle.textContent = item.title;
+  headingSub.textContent = item.subTitle;
+  content.textContent = item.content;
 
-    divParent.appendChild(divContent);
-    return classParent.appendChild(divParent);
+  divContent.appendChild(headingSub);
+  divContent.appendChild(headingTitle);
+  divContent.appendChild(classImage);
+  divContent.appendChild(content);
+
+  divParent.appendChild(divContent);
+  return classParent.appendChild(divParent);
 });
-
-
